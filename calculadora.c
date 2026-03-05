957f8bf6c090196fa5ceaa38b184ed8e90773f81
VictorTaborda1 <vtaborda@unipacifico.edu.co>
2026-03-05 16:34:20 -0500

    conectando con github

---
+++ calculadora.c
@@ -0,0 +1,64 @@
+/*encabezado- cabezera*/
+#include <stdio.h>
+
+/*Declaracion Funciones y Variables*/
+double sumar();
+double restar();
+double multiplicar();
+double dividir();
+double total;
+
+/*Logica del negocio - Logica del Modelo*/
+int main(){
+    int opcion;
+    do{
+        printf("1.sumar\n");
+        printf("2.restar\n");
+        printf("3.multiplicar\n");
+        printf("4.dividir\n");
+        printf("5.salir\n");
+        scanf("%d", &opcion);
+        switch (opcion){
+            case 1: sumar ();break;
+            case 2: restar ();break;
+            case 3: multiplicar ();break;
+            case 4: dividir ();break;
+        }
+
+    } while (opcion !=5);
+
+        return 0;
+}
+
+double sumar(){
+    double x, y;
+    printf("Digite el primer numero");
+    scanf("%d", &x);
+    printf("Digite el segundo numero");
+    scanf("%d", &y);
+    total= x + y;
+}
+double restar(){
+    double x, y;
+    printf("Digite el primer numero");
+    scanf("%d", &x);
+    printf("Digite el segundo numero");
+    scanf("%d", &y);
+    total= x - y;
+}
+double multiplicar(){
+    double x, y;
+    printf("Digite el primer numero");
+    scanf("%d", &x);
+    printf("Digite el segundo numero");
+    scanf("%d", &y);
+    total= x * y;
+}
+double dividir(){
+    double x, y;
+    printf("Digite el primer numero");
+    scanf("%d", &x);
+    printf("Digite el segundo numero");
+    scanf("%d", &y);
+    total= x / y;
+}
